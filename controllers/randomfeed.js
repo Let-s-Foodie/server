2b75997c9f920c0c0d40c170c16215fcbb4a70ce
Geonhyeong Park <Abby@Geonhyeongs-MacBook-Air.local>
2021-05-12 20:50:47 -0700

    request yelp api

--- controllers/randomfeed.js
+++ controllers/randomfeed.js
@@ -2,18 +2,47 @@ require("dotenv").config();
 const yelp = require('yelp-fusion');
 const apiKey = process.env.API_KEY;
 const searchRequest = {
-    term: 'food',
+   
     location: 'fremont, ca',
-    limit: '50'
+    limit: '5'
 }
 const client = yelp.client(apiKey);
 
+exports.getDetail = (req,res,next) => {
+    console.log(req.body.title)
+    const category = req.body.category;
+    const name = req.body.title;
+    const area = req.body.area;
+    const latitude = "37.80468";
+    const longitude = "-122.27119";
+    const categoryarr = [];
+    categoryarr.push(category);
+    categoryarr.push(area);
+   
+    searchRequest.categories = categoryarr
+    searchRequest.term = area
+    // searchRequest.latitude = latitude;
+    // searchRequest.longitude = longitude;
+    
+    console.log(searchRequest)
+    client.search(searchRequest)
+        .then(res => {
+            const detailResult = res.jsonBody
+            return detailResult;
+        })
+        .catch((e)=>{console.log(e)})
+        .then(data => {
+            res.status(200).json({message: 'get detail title',data: data})
+        }
 
+        )
+
+}
 exports.getRandom = (req,res,next) => {
    client.search(searchRequest)
         .then(res => {
         const firstResult = res.jsonBody
-        console.log(res.jsonBody.businesses)
+       // console.log(res.jsonBody.businesses)
         const prettyJson = JSON.stringify(firstResult, null,4);
         return firstResult;
     })
--- routes/randomfeed.js
+++ routes/randomfeed.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const randomFeedController = require('../controllers/randomfeed');
 //Get /random/feeds
 router.get('/feeds',randomFeedController.getRandom);
-
+//Get /random/detail
+router.post('/detail',randomFeedController.getDetail)
 module.exports = router;
