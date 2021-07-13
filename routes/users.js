48504c7fd96c2b3a81ab5a0f8758f637dc6dd2c7
Joon Jongmin Kim <joon@joons-mbp.lan>
2021-07-12 23:20:24 -0700

    add authcheck

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const { add, update } = require("../controllers/users");
-const { authCheck, adminCheck } = require("../middleware/auth");
+const { authCheck } = require("../middleware/auth");
 
 //router.post("/", authCheck, adminCheck, add);
 router.post("/signup", add);
-router.put('/:id', update)
+router.put('/:id', authCheck, update)
 module.exports = router;
